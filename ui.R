b2d979d1eccacce33e0a3f8004f5f359a289e964
SeaRhythms <searhythms@gmail.com>
2014-09-16 15:22:14 -0700

    Update range and instructions in ui.R

--- ui.R
+++ ui.R
@@ -10,8 +10,14 @@ shinyUI(fluidPage(
     sidebarLayout(
         # Get the length of movie     
         sidebarPanel(
-            numericInput('mlength', 'Length of movie in minutes',90,min = 1,max = 5300)                                    
-        ),
+            numericInput('mlength', 'Length of movie in minutes',90,min = 60,max = 240),
+            br(),
+            br(),
+            strong("Instructions"),
+            p("Enter the length of a movie in minutes directly or use the up and down",
+              "arrow keys and see the prediction overlayed on a histogram", 
+              " of all the data. The prediction comes from a linear regression of ",
+              " all the movie ratings.")
         
         # Show a histogram with the prediction 
         mainPanel(
